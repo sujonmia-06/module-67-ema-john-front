fd3852bd5928e956c9188209d1c615f6a8369edf
sujon-mia <sujonmia.0612@gmail.com>
2022-04-12 16:26:01 +0600

    henlo

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -20,8 +20,9 @@
 
 /**
  * Firebase hosing steps
- * 1. npm install -g firebase-tools
- * 2. firebase login
- * 3. firebase init
- * 4. firebase deploy
+ * 1. npm install -g firebase-tools (1 time for pc)
+ * 2. firebase login (1 time for pc)
+ * 3. firebase init (1 time for each project)
+ * 4. npm run build (every time to deploy => build the project)
+ * 5. firebase deploy (every time to deploy)
  */
